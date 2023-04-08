dd53382f33d264e195fab8b5f61b49e675016bc4
isma3l <karlo.ismael@gmail.com>
2023-04-08 12:37:09 +0200

    💄 mejora en el header

--- src/components/molecules/header/Header.component.tsx
+++ src/components/molecules/header/Header.component.tsx
@@ -3,7 +3,7 @@ import { Loader } from "../../atoms";
 
 const HeaderComponent = () => {
   return (
-    <div className="border-b-2 border-gray-300 py-4 flex justify-between">
+    <div className="border-b-2 border-gray-300 py-3 flex justify-between">
       <Link className="text-3xl font-semibold text-sky-600" to="/">
         Podcaster
       </Link>
