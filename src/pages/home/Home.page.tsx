b848f05c059219a65943f5968996880ecd33e4b7
isma3l <karlo.ismael@gmail.com>
2023-04-08 23:05:43 +0200

    ✨ se completa el home

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,73 +1,37 @@
 import { FilterComponent, PodcastListComponent } from "@/features/home";
 import { PodcastItemInterface } from "@/models";
-import { useState } from "react";
-
-const podcasts: PodcastItemInterface[] = [
-  {
-    title: "pedro",
-    author: "song explorer",
-    id: "1q22",
-    urlImage:
-      "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
-    description: "",
-  },
-  {
-    title: "manuel",
-    author: "lala mi cancion",
-    id: "1q",
-    urlImage:
-      "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
-    description: "",
-  },
-  {
-    title: "jose",
-    author: "lala explorer",
-    id: "1eesq",
-    urlImage:
-      "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
-    description: "",
-  },
-  {
-    title: "ricardo",
-    author: "song pollo",
-    id: "1dwfffq",
-    urlImage:
-      "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
-    description: "",
-  },
-  {
-    title: "kexp",
-    author: "song explorer",
-    id: "1212q",
-    urlImage:
-      "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
-    description: "",
-  },
-  {
-    title: "kexp",
-    author: "song explorer",
-    id: "1tfhhhwq",
-    urlImage:
-      "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
-    description: "",
-  },
-  {
-    title: "kexp",
-    author: "song explorer",
-    id: "1q3242vv",
-    urlImage:
-      "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
-    description: "",
-  },
-];
+import { useEffect, useState } from "react";
+import { fetchPodcasts } from "@/services";
+import { useLoaderContext } from "@/hooks";
 
 const HomePage = () => {
+  const [error, setError] = useState(false);
   const [query, setQuery] = useState<string>("");
-  const [filteredPodcasts, setFilteredPodcasts] =
-    useState<PodcastItemInterface[]>(podcasts);
+  const { state, showLoading, hideLoading } = useLoaderContext();
+  const [podcasts, setPodcasts] = useState<PodcastItemInterface[]>([]);
+  const [filteredPodcasts, setFilteredPodcasts] = useState<
+    PodcastItemInterface[]
+  >([]);
+
+  useEffect(() => {
+    const getPodcasts = async () => {
+      try {
+        showLoading();
+        const data = await fetchPodcasts();
+        setPodcasts(data ?? []);
+        setFilteredPodcasts(data ?? []);
+      } catch (err: unknown) {
+        setError(true);
+      } finally {
+        hideLoading();
+      }
+    };
+
+    void getPodcasts();
+  }, []);
 
   const handleChange = (value: string) => {
-    const filteredList: PodcastItemInterface[] = podcasts.filter(
+    const filteredList: PodcastItemInterface[] = podcasts?.filter(
       (podcast: PodcastItemInterface) => {
         return (
           podcast.title.toLowerCase().search(value.toLowerCase()) !== -1 ||
@@ -81,14 +45,22 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col py-6">
-      <div className="self-end pe-6">
-        <FilterComponent
-          label={filteredPodcasts.length}
-          value={query}
-          onChange={handleChange}
-        />
-      </div>
-      <PodcastListComponent podcasts={filteredPodcasts} />
+      {error ? (
+        "Error"
+      ) : state.loading ? (
+        "Cargando..."
+      ) : (
+        <>
+          <div className="self-end pe-6">
+            <FilterComponent
+              label={filteredPodcasts.length}
+              value={query}
+              onChange={handleChange}
+            />
+          </div>
+          <PodcastListComponent podcasts={filteredPodcasts} />
+        </>
+      )}
     </div>
   );
 };
