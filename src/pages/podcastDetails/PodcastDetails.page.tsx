6426d6ea4d0b85aeaae2edfe0e4f435427f148a6
isma3l <karlo.ismael@gmail.com>
2023-04-09 13:08:17 +0200

    💄 se actualliza la pagina de los detalles para que muestre parte de los detalles de un poscast

--- src/pages/podcastDetails/PodcastDetails.page.tsx
+++ src/pages/podcastDetails/PodcastDetails.page.tsx
@@ -1,26 +1,63 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import { PodcastCardComponent } from "@/features/detailPodcast/components";
 import { useLoaderContext } from "@/hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PodcastDetailsInterface } from "@/models";
+import { fetchPodcastDetails } from "@/services/podcastDetailsService";
+
+const podcastDetailEmpty: PodcastDetailsInterface = {
+  episodes: [],
+  podcast: {
+    id: "",
+    title: "",
+    author: "",
+    urlImage: "",
+  },
+};
 
 const PodcastDetailsPage = () => {
-  const { state, showLoading, hideLoading } = useLoaderContext();
+  const urlParams = useParams();
   const [error, setError] = useState(false);
-  const [podcastDetails, setpodcastDetails] =
-    useState<PodcastDetailsInterface | null>();
+  const { state, showLoading, hideLoading } = useLoaderContext();
+  const [podcastDetails, setPodcastDetails] =
+    useState<PodcastDetailsInterface>(podcastDetailEmpty);
+
+  const { podcast, episodes } = podcastDetails;
+
+  useEffect(() => {
+    const getPodcastDetails = async () => {
+      try {
+        showLoading();
+        const data = await fetchPodcastDetails(urlParams.podcastId ?? "");
+        if (data) setPodcastDetails(data);
+        console.log("dataaa: ", data);
+      } catch (err: unknown) {
+        setError(true);
+      } finally {
+        hideLoading();
+      }
+    };
+
+    void getPodcastDetails();
+  }, []);
 
   return (
     <div className="flex py-6 justify-between">
-      <PodcastCardComponent
-        title={""}
-        author={""}
-        urlImage={
-          "https://ssl-static.libsyn.com/p/assets/6/5/5/a/655a79b7f130e7cde5bbc093207a2619/JBN_Apple_Podcast.jpg"
-        }
-        description={""}
-      />
-      <Outlet context={{ podcastDetails }} />
+      {error ? (
+        "Error"
+      ) : state.loading ? (
+        "Cargando..."
+      ) : (
+        <>
+          <PodcastCardComponent
+            title={podcast.title}
+            author={podcast.author}
+            urlImage={podcast.urlImage}
+            description={podcast?.description ?? ""}
+          />
+          <Outlet context={{ podcastDetails }} />
+        </>
+      )}
     </div>
   );
 };
