0a420ef9044bd4a7feac28b339afe5a2af4b63eb
isma3l <karlo.ismael@gmail.com>
2023-04-09 11:26:56 +0200

    ✨ se agrega componente tabla

--- src/features/detailPodcast/components/episodesTable/EpisodesTable.component.tsx
+++ src/features/detailPodcast/components/episodesTable/EpisodesTable.component.tsx
@@ -1,9 +1,41 @@
-/* type EpisodeTableComponentProps = {
-    
-} */
+import { EpisodeInterface } from "@/models";
 
-const EpisodeTableComponent = () => {
-  return <div>prueba</div>;
+type EpisodeTableComponentProps = {
+  episodes: EpisodeInterface[];
+};
+
+const EpisodeTableComponent = ({ episodes }: EpisodeTableComponentProps) => {
+  return (
+    <div className="px-6 py-2 border border-gray-100 rounded-xl shadow-lg">
+      <table className="table-auto w-full">
+        <thead>
+          <tr className="border-b border-slate-300">
+            <th className="w-8/12 text-start py-4">Title</th>
+            <th className="text-start py-4">Date</th>
+            <th className="text-center py-4">Duration</th>
+          </tr>
+        </thead>
+        <tbody className="divide-y">
+          {episodes.map((episode) => {
+            return (
+              <tr
+                className="odd:bg-white even:bg-slate-50 border-b border-slate-300"
+                key={episode.id}
+              >
+                <td className="py-4">
+                  <a href="" className="text-blue-600 font-medium">
+                    {episode.title}
+                  </a>
+                </td>{" "}
+                <td className=" py-4">{episode.date}</td>
+                <td className="text-center py-4">{episode.duration}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default EpisodeTableComponent;
--- src/features/detailPodcast/components/index.ts
+++ src/features/detailPodcast/components/index.ts
@@ -1,3 +1,4 @@
 export { default as PodcastDetailsComponent } from "./details/PodcastDetails.component";
 export { default as PodcastEpisodeComponent } from "./episode/PodcastEpisode.component";
 export { default as PodcastCardComponent } from "./card/PodcastCard.component";
+export { default as EpisodeTableComponent } from "./episodesTable/EpisodesTable.component";
