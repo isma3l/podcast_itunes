8649a7ac720a5e366d43072b764a15a8daa7f8c3
isma3l <karlo.ismael@gmail.com>
2023-04-09 21:41:58 +0200

    💄 ➕  se agrega rss-parser y se corrige la obtencion de dependencias con craco

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,28 @@
 const path = require('path');
+const webpack = require("webpack");
+
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+    configure: {
+      resolve: {
+        fallback: {
+          process: require.resolve("process/browser"),
+          http: require.resolve("stream-http"),
+          https: require.resolve("https-browserify"),
+          url: require.resolve("url"),
+          timers: require.resolve("timers-browserify"),
+          buffer: require.resolve("buffer"),
+        },
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+          process: "process/browser",
+        }),
+      ],
     },
   },
 };
