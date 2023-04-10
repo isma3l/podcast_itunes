d99cd69291ab2234f2c261e8bbe38fa3a2d5ecdc
isma3l <karlo.ismael@gmail.com>
2023-04-10 13:20:37 +0200

    💄 mejora visual de la card del listado de podcasts

--- src/features/home/components/item/PodcastItem.component.tsx
+++ src/features/home/components/item/PodcastItem.component.tsx
@@ -9,20 +9,22 @@ type PodcastItemComponentProps = {
 const PodcastItemComponent = ({ podcast }: PodcastItemComponentProps) => {
   return (
     <Link to={RouteKeys.podcast(podcast.id)}>
-      <div className="relative w-64 h-44 border border-gray-100 rounded-xl shadow-lg">
-        <div className="absolute bottom-[92px] left-14">
-          <img
-            className="block mx-auto h-36 rounded-full sm:mx-0 sm:shrink-0"
-            src={podcast.urlImage}
-          />
-        </div>
-        <div className="absolute top-1/2 w-full">
-          <div className="text-sm leading-5 font-medium text-black px-3 py-2 text-center uppercase">
-            {podcast.title}
+      <div className="flex flex-col w-64 h-60">
+        <div className="rounded-xl shadow-lg border border-gray-100 mt-16">
+          <div className="relative h-20 w-full">
+            <img
+              className="absolute mx-auto h-36 rounded-full sm:mx-0 sm:shrink-0 -top-16 left-14"
+              src={podcast.urlImage}
+            />
           </div>
-          <p className="text-slate-500 text-center text-sm px-3">
-            Author: {podcast.author}
-          </p>
+          <div className="relative mb-6">
+            <div className="text-sm leading-5 font-medium text-black px-3 py-2 text-center uppercase">
+              {podcast.title}
+            </div>
+            <p className="text-slate-500 text-center text-sm px-3">
+              Author: {podcast.author}
+            </p>
+          </div>
         </div>
       </div>
     </Link>
