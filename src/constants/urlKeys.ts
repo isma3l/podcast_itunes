b5b1f46c47692f3aca29d66abf2982bce186583e
isma3l <karlo.ismael@gmail.com>
2023-04-14 19:04:26 +0200

    🐛 fixes proxy server URL that fixes CORS issues

--- src/constants/urlKeys.ts
+++ src/constants/urlKeys.ts
@@ -1,9 +1,10 @@
-const BASE_URL = "https://server-proxy-podcasts.vercel.app";
+const BASE_URL = "https://server-proxy.vercel.app/api";
+const ITUNES_URL = "https://itunes.apple.com";
 
 export const urlKeys = {
-  baseUrl: BASE_URL,
-  podcastsUrl: "/api/podcasts",
-  podcastDetailsUrl: (podcastId: string) => `/api/podcasts/${podcastId}`,
-  podcastEpisodesUrl: (feedUrl: string) =>
-    `${BASE_URL}/api/episodes?feedurl=${feedUrl}`,
+  baseUrl: `${BASE_URL}`,
+  podcastsUrl: `?url=${ITUNES_URL}/us/rss/toppodcasts/limit=100/genre=1310/json`,
+  podcastDetailsUrl: (podcastId: string) =>
+    `?url=${ITUNES_URL}/lookup?id=${podcastId}`,
+  podcastEpisodesUrl: (feedUrl: string) => `${BASE_URL}?url=${feedUrl}`,
 };
